af3c398fe0c6e933f41384ef245980323a933614
Furlaneto1995 <54684464+Furlaneto1995@users.noreply.github.com>
2026-09-08 16:41:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'encaixes-v33'; // Subir este número a cada publicação força a atualização
+var CACHE_NAME = 'encaixes-v34'; // Subir este número a cada publicação força a atualização
 
 var urlsToCache = [
     './',
